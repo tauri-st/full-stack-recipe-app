fb4b25fdad07faec3ee90442fe031a87ce9c12ac
tauri_s <tauristclaire@gmail.com>
2024-06-12 17:12:43 -0700

    Asssign location for recipes database

--- api/api.py
+++ api/api.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
