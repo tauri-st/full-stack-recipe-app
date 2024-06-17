2f89aaa371bae6b9cfeaa6c9b1af5f620ac65520
tauri_s <tauristclaire@gmail.com>
2024-06-17 14:11:43 -0700

    Fix indentation

--- api/api.py
+++ api/api.py
@@ -7,17 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 db = SQLAlchemy(app)
 
 class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    ingredients = db.Column(db.String(500), nullable=False)
-    instructions = db.Column(db.Text, nullable=False)
-    description = db.Column(db.Text, nullable=True,
-                            default='Delicious. You need to try it!')
-    image_url = db.Column(db.String(500), nullable=True,
-                       default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
-    servings = db.Column(db.Integer, nullable=False)
-    def __repr__(self):
-        return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  ingredients = db.Column(db.String(500), nullable=False)
+  instructions = db.Column(db.Text, nullable=False)
+  description = db.Column(db.Text, nullable=True, default='Delicious. You need to try it!')
+  image_url = db.Column(db.String(500), nullable=True, default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+  servings = db.Column(db.Integer, nullable=False)
+  def __repr__(self):
+    return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
     
 #with app.app_context():
   #db.create_all()
@@ -29,16 +27,16 @@ def get_all_recipes():
   recipes = Recipe.query.all()
   recipe_list = []
   for recipe in recipes:
-     recipe_list.append({
-        'id': recipe.id,
-        'title': recipe.title,
-        'ingredients': recipe.ingredients,
-        'instructions': recipe.instructions,
-        'description': recipe.description,
-        'image_url': recipe.image_url,
-        'servings': recipe.servings
-     })
+    recipe_list.append({
+      'id': recipe.id,
+      'title': recipe.title,
+      'ingredients': recipe.ingredients,
+      'instructions': recipe.instructions,
+      'description': recipe.description,
+      'image_url': recipe.image_url,
+      'servings': recipe.servings
+    })
   return jsonify(recipe_list) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
