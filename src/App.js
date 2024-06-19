22b45de566a5702a1fa35c71d604f7a325dcba59
tauri_s <tauristclaire@gmail.com>
2024-06-19 14:18:57 -0700

    Import useState and create state for recipes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
   return (
     <div className='recipe-app'>
       <Header />
