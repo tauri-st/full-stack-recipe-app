d323a51df0bfb91785308e5be3129ca644337b7f
tauri_s <tauristclaire@gmail.com>
2024-06-19 15:47:48 -0700

    Change alt attribute for RecipeExerpt image to refer to recipe title

--- src/components/RecipeExerpt.js
+++ src/components/RecipeExerpt.js
@@ -7,7 +7,7 @@ import React from 'react';
 const RecipeExerpt = ( {recipe} ) => {
     return (
         <article className="recipe-card">
-            <figure><img src={recipe.image_url} alt="recipe image"/></figure>
+            <figure><img src={recipe.image_url} alt={recipe.title}/></figure>
             <h2>{recipe.title}</h2>
             <p className="flex-spacing">Description: {recipe.description}</p>
             <button>View</button>
