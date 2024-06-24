b42bb99d08fab535c4d923f4bcf21b5be4e58381
tauri_s <tauristclaire@gmail.com>
2024-06-24 16:25:08 -0700

    Conditionally render NewRecipeForm

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
   return (
     <div className='recipe-app'>
       <Header />
+      {showNewRecipeForm && <NewRecipeForm />}
       {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
       {status === "error" && (
         <p>"Something went wrong"</p>
