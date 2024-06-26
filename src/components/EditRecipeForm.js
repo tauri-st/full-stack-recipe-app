02d144317546c9088de62a1045110eb895efe66f
tauri_s <tauristclaire@gmail.com>
2024-06-26 15:09:48 -0700

    Add onChange event to EditRecipeForm fields

--- src/components/EditRecipeForm.js
+++ src/components/EditRecipeForm.js
@@ -7,17 +7,17 @@ const EditRecipeForm = ( { selectedRecipe, handleCancel } ) => {
       <button className='cancel-button' onClick={handleCancel}>Cancel</button>
       <form>
         <label>Title</label>
-        <input type='text' name='title' value={selectedRecipe.title} onChange='' required />
+        <input type='text' name='title' value={selectedRecipe.title} onChange={(e) => onUpdateForm(e, "update")} required />
         <label>Ingredients</label>
-        <textarea name='ingredients' value={selectedRecipe.ingredients} onChange='' required />
+        <textarea name='ingredients' value={selectedRecipe.ingredients} onChange={(e) => onUpdateForm(e, "update")} required />
         <label>Instructions</label>
-        <textarea name='instructions' value={selectedRecipe.instructions} onChange='' required />
+        <textarea name='instructions' value={selectedRecipe.instructions} onChange={(e) => onUpdateForm(e, "update")} required />
         <label>Description</label>
-        <textarea name='description' value={selectedRecipe.description} onChange='' required />
+        <textarea name='description' value={selectedRecipe.description} onChange={(e) => onUpdateForm(e, "update")} required />
         <label>Image</label>
-        <input type='text' name='image_url' value={selectedRecipe.image_url} onChange='' required />
+        <input type='text' name='image_url' value={selectedRecipe.image_url} onChange={(e) => onUpdateForm(e, "update")} required />
         <label>Servings</label>
-        <input type='number' name='servings' value={selectedRecipe.servings} onChange='' required />
+        <input type='number' name='servings' value={selectedRecipe.servings} onChange={(e) => onUpdateForm(e, "update")} required />
         <button type='submit'>Update Recipe</button>
       </form>
     </div>
