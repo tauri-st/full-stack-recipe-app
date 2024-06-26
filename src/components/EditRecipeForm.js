af5aeae4900b9cdccdf76f1632d9a3b7ccc41be5
tauri_s <tauristclaire@gmail.com>
2024-06-26 14:02:19 -0700

    Destructure selectedRecipe and handleCancel in the EditRecipeForm

--- src/components/EditRecipeForm.js
+++ src/components/EditRecipeForm.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const EditRecipeForm = () => {
+const EditRecipeForm = ( { selectedRecipe, handleCancel } ) => {
   return (
     <div className='recipe-form'>
       <h2>Edit "{}"</h2>
-      <button className='cancel-button'>Cancel</button>
+      <button className='cancel-button' onClick={handleCancel}>Cancel</button>
       <form>
         <label>Title</label>
-        <input type='text' name='title' value='' onChange='' required />
+        <input type='text' name='title' value={selectedRecipe.title} onChange='' required />
         <label>Ingredients</label>
-        <textarea name='ingredients' value='' onChange='' required />
+        <textarea name='ingredients' value={selectedRecipe.ingredients} onChange='' required />
         <label>Instructions</label>
-        <textarea name='instructions' value='' onChange='' required />
+        <textarea name='instructions' value={selectedRecipe.instructions} onChange='' required />
         <label>Description</label>
-        <textarea name='description' value='' onChange='' required />
+        <textarea name='description' value={selectedRecipe.description} onChange='' required />
         <label>Image</label>
-        <input type='text' name='image_url' value='' onChange='' required />
+        <input type='text' name='image_url' value={selectedRecipe.image_url} onChange='' required />
         <label>Servings</label>
-        <input type='number' name='servings' value='' onChange='' required />
+        <input type='number' name='servings' value={selectedRecipe.servings} onChange='' required />
         <button type='submit'>Update Recipe</button>
       </form>
     </div>
