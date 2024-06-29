96951399374db821d1f5422b39318a07abee005f
tauri_s <tauristclaire@gmail.com>
2024-06-28 17:05:42 -0700

    Add returned JSX to ConfirmationModal component

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const ConfirmationModal = () => {
   return (
-    <div>ConfirmationModal</div>
+    <div className='confirm'>
+        <div className='confirm-content'>
+            <p></p>
+            <button className='cancel-button'>
+                Cancel
+            </button>
+            <button className='delete-button'>
+                Delete
+            </button>
+        </div>
+    </div>
   )
-}
+};
 
 export default ConfirmationModal;
