a323b2271f8eaf27f949777b54871be4cc2b6461
tauri_s <tauristclaire@gmail.com>
2024-06-28 17:20:24 -0700

    Destructure props in ConfirmationModal and wire up onClick handlers

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ConfirmationModal = () => {
+const ConfirmationModal = ( {message, onCancel, onConfirm} ) => {
   return (
     <div className='confirm'>
         <div className='confirm-content'>
-            <p></p>
-            <button className='cancel-button'>
+            <p>{message}</p>
+            <button className='cancel-button' onClick={onCancel}>
                 Cancel
             </button>
-            <button className='delete-button'>
+            <button className='delete-button' onClick={onConfirm}>
                 Delete
             </button>
         </div>
