94a7ca8780318730f9d72ee758484a756e0b617b
tauri_s <tauristclaire@gmail.com>
2024-07-03 14:02:42 -0700

    Pass the recipe description to truncateText

--- src/components/RecipeExerpt.js
+++ src/components/RecipeExerpt.js
@@ -6,7 +6,7 @@ const RecipeExerpt = ( {recipe, handleSelectRecipe} ) => {
         <article className="recipe-card">
             <figure><img src={recipe.image_url} alt={recipe.title}/></figure>
             <h2>{recipe.title}</h2>
-            <p className="flex-spacing">Description: {recipe.description}</p>
+            <p className="flex-spacing">Description: {truncateText(recipe.description)}</p>
             <button onClick={() => handleSelectRecipe(recipe)}>View</button>
         </article>
     )
