cd2e3dfdf2e9ccfecf4ff09a5f0a9e7a3a4b0e9e
tauri_s <tauristclaire@gmail.com>
2024-07-03 13:05:59 -0700

    Stub out truncateText function with pseudocode

---
+++ src/helpers/utils.js
@@ -0,0 +1,9 @@
+const truncateText = (text, numWords=20) => { 
+    /*
+    TODO: Check if the total words shorter or equal to numWords, return as is
+    TODO: If yes, return as is
+    TODO: If not, return first 20 words
+        TODO: Add ellipsis
+    * Will need to go from string to an array back to a string again
+    */
+};
