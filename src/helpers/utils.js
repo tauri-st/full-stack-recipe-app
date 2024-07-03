494c0639756387fe48b5460065678da0072a85f0
tauri_s <tauristclaire@gmail.com>
2024-07-03 13:31:09 -0700

    Add statements to return max words or less

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -9,6 +9,8 @@ const truncateText = (text, numWords=20) => {
     if (text.length <= numWords) {
         return text;
     } else {
-        
+        const textArray = text.split(" ");
+        const maxWords = textArray.slice(0, numWords);
+        return maxWords.join(" ") + "...";
     }
 };
