0a65869ba742c8563e14f7c553d1d032e8ae31fd
tauri_s <tauristclaire@gmail.com>
2024-07-05 16:18:58 -0700

    Add if else clause for success and errors

--- src/helpers/toastHelper.js
+++ src/helpers/toastHelper.js
@@ -1,5 +1,14 @@
 import {toast} from 'react-toastify';
 
 const displayToast = (message, type=success) => { 
-
+    const options = {
+        position: "bottom-right",
+        autoClose: 2000
+    };
+    if (type==="success") {
+        return toast.success(message, options);
+    }
+    else if (type==="error") {
+        return toast.error(message, options);
+    }
 };
